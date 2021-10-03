7d02770bf00f6b3504a735dbf00048839bdfb293
yn1323 <yn1323@gmail.com>
2021-10-03 23:47:41 +0900

    no message

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -7,7 +7,7 @@ export default function NotFoundScreen({
 }: RootStackScreenProps<'NotFound'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
+      <Text style={styles.title}>This screen doesnt exist.</Text>
       <TouchableOpacity
         onPress={() => navigation.replace('Root')}
         style={styles.link}
